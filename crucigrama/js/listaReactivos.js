f5c6f0f4812cf89cb3200a84fcbfdb070d30e6b1
Karen <maykaren20ram@gmail.com>
2020-03-23 20:57:00 -0600

    Version master: presentacion 2020

---
+++ crucigrama/js/listaReactivos.js
@@ -0,0 +1,48 @@
+/*
+------------------------Estructura de listaReactivos----------------
+var palabraspro =[
+    ["palabra1","Definición corta"],
+    ["palabra2","Definición corta"],
+    ["palabra3","Definición corta"]
+];
+NOTA: Longitud mínima de las palabras 2 y máxima 25
+NOTA: Automáticamente se crea el crucigrama y define las palabras que estarán en forma vertical y horizontal.
+NOTA: Acepta palabras con acento y diéresis, de acuerdo como se escriba la palabra será tomada como correcta.
+Ejemplo:
+    ["Programacion","Ciencia de las computadoras."] 
+        Será tomada como correcta programacion SIN acento.
+
+    ["Programación","Ciencia de las computadoras."]
+        Será tomada como correcta programacion CON acento.
+*/
+
+var palabraspro = [
+    ["Separado", "Cuando dos elementos están lejos."],
+    ["Cuidado", "Mantenerse alerta."],
+    ["ONU", "Organización de las Naciones Unidas."],
+    ["Realidad", "Todo lo real."],
+    ["Amable", "Persona que siempre te trata bien."],
+    ["Bienestar", "Tener todo en armonía."],
+    ["Camino", "Cuando regresamos a casa."],
+    ["Programación", "Ciencia de las computadoras."],
+    ["Constitución", "Firmada en 1917."],
+    ["Cuchillo", "Utensilio utilizado para cortar."],
+    ["Celular", "Permite comunicarnos de manera sencilla."],
+    ["Caballo", "Animal con cuatro patas."],
+    ["Corazón", "Late y late para dar vida."],
+    ["Perseverancia", "Seguir adelante apesar de todo."],
+    ["Castillo", "Construcción de la Edad Media."],
+    ["Muralla", "Para que no pase nadie."],
+    ["Computadora", "Conjunto de dispositivos digitales."],
+    ["Lapicero", "Utilizado para escribir."],
+    ["Ingeniería", "El arte del ingenio para resolver problemas."],
+    ["Laboratorio", "Lugar para experimentar."],
+    ["Líbano", "Flor de loto."],
+    ["Esternocleidomastoideo", "Músculo ubicado en la zona anterior y lateral del cuello."],
+    ["Claustrofobia", "Miedo a los espacios cerrados."],
+    ["Ventilador", "Algo que da aire."],
+    ["Tornado", "Catástrofe con vientos."],
+    ["Iglesia", "Lugar de rezo."],
+    ["Miguel Hidalgo", "Padre de la patria (México)."],
+    ["Cálculo diferencial", "Materia de ingeniería."]
+];
